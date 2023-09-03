f7f93ff37fa1539b9068e020a67778130910b7db
황은지 <@192.168.123.104>
2023-09-03 19:16:15 +0900

    9/03 spring 을 이용한 게시판작성1

---
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,43 @@
+package com.study.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+
+@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	//글작성
+	public void write(Board board) {
+		
+		boardRepository.save(board);
+	}
+	
+	
+	
+	//게시글 리스트 처리
+	public List<Board> boardList(){
+		
+		return boardRepository.findAll();
+	}
+	
+	//특정 게시글 불러오기
+	public Board boardview(Integer id) {
+		
+		
+		return boardRepository.findById(id).get();
+	}
+	
+	//특정 게시글 삭제
+	public void boardDelete(Integer id) {
+		
+		boardRepository.deleteById(id);
+	}
+}
